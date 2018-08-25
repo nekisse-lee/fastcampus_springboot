1f87eda604e468958aaa79dadf3725ec5f44fe9c
nekisse <lsh891224@gmail.com>
2018-08-25 15:43:44 +0900

    로그인 처리 완료

--- jpaexam/src/main/java/examples/boot/jpaexam/controller/MemberController.java
+++ jpaexam/src/main/java/examples/boot/jpaexam/controller/MemberController.java
@@ -1,12 +1,17 @@
 package examples.boot.jpaexam.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/members")
 public class MemberController {
 
-
+    @GetMapping("/login")
+    public String login() {
+        //resources/templates/members/login.html
+        return "members/login";   //view name을 리턴
+    }
 
 }
